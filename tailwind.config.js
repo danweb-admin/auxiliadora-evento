7f2593d67e91e92215d2c52ac9df87260a6404ed
William Nascimento <william.nascimento2010@gmail.com>
2026-03-25 21:36:51 -0300

    mudança de cor

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,22 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          50: '#eef0ff',
+          100: '#d9dcff',
+          200: '#b3b7ff',
+          300: '#8c92ff',
+          400: '#666dff',
+          500: '#4d54f0',
+          600: '#3936CD',
+          700: '#2f2bb0',
+          800: '#252193',
+          900: '#1b1776'
+        }
+      }
+    },
   },
   plugins: [],
 }
